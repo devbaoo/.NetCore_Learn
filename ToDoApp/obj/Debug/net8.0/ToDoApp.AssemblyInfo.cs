24e6b623247fc942ba631a4d9349844596fb9c49
Khắc Bảo <caokhacbao0712@gmail.com>
2025-04-06 11:28:20 +0700

    update db aws

--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb3ff92d1d03cb32aebcafb895eb8618e7e8ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93cda712d163c72049e2273849cce915a829eb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
